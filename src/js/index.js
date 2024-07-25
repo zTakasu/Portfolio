7dc22ced13b6f1e4fc5628e36035536a37c6bb03
zTakasu <andreasschweiger1@t-online.de>
2024-07-25 14:43:38 +0200

    visual adjustments

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,23 @@
-lightSwitch = document.getElementById("lightSwitch");
+lightSwitch = document.querySelector("#lightSwitch");
 lightBeam = document.getElementById("lightBeam");
+lightSwitchInner = document.querySelector("#lightSwitchInner");
+lightSwitchShadow = document.querySelector("#lightSwitchShadow");
+lightBulb = document.querySelector("#lightBulb");
 
 lightSwitch.addEventListener("click", switchLight);
 
 function switchLight() {
-  document.getElementById;
-  if (lightBeam.style.visibility === "visible") {
+  console.log("got pressed");
+  if (
+    lightBeam.style.visibility === "visible" ||
+    lightBeam.style.visibility === ""
+  ) {
     lightBeam.style.visibility = "hidden";
+    lightSwitchShadow.style.transform = "translateY(-24px)";
+    lightBulb.style.opacity = "0.5";
   } else {
     lightBeam.style.visibility = "visible";
+    lightSwitchShadow.style.transform = "translateY(0px)";
+    lightBulb.style.opacity = "1";
   }
 }
